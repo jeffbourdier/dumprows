4e9c74fd5040e4537d485970e0ac8cd978d4489d
root <root@mnbvfcsadev08.corp.pvt>
2021-03-21 16:09:17 -0500

    correction for <td> tag attributes

--- dumprows.c
+++ dumprows.c
@@ -256,7 +256,7 @@ void format_results(char * output, char ** addl_head_ptr, const char ** body_att
     for (m = 1, r = q - i; (j = text_search(r, "<td")) && (r += j) < q; ++m)
     {
       /* Advance the content pointer to the character after the <td> tag.  (There may be attributes in this tag.) */
-      while (*++r != '>');
+      while (*r++ != '>');
 
       /* Determine whether or not this cell contains valid GeoJSON geometry. */
       if (g > 0)
